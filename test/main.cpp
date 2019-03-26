103ad92e7f0601a4f879de27ebc4c90589699063
iibrahimli <imranibrahimli98@gmail.com>
2019-03-26 21:45:53 +0400

    add text test runner

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include "test_bs_grid.hpp"
+// #include "test_battleship.hpp"
 
-using namespace std;
+
+CPPUNIT_TEST_SUITE_REGISTRATION(test_bs_grid);
+// CPPUNIT_TEST_SUITE_REGISTRATION(test_battleship);
+
 
 int main(){
-
+    CppUnit::Test *test = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+    CppUnit::TextTestRunner runner;
+    runner.addTest(test);
+    runner.run();
     return 0;
 }
