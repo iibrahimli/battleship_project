de67a656ac5180acccf708d707a7b49875bae975
iibrahimli <imranibrahimli98@gmail.com>
2019-03-26 19:44:15 +0400

    rename bs_grid tests header

---
+++ test/test_bs_grid.hpp
@@ -0,0 +1,25 @@
+#ifndef TEST_BS_GRID_HPP
+#define TEST_BS_GRID_HPP
+
+#include <string>
+#include "cppunit/TestCase.h"
+#include "cppunit/TestSuite.h"
+#include "cppunit/TestCaller.h"
+#include "cppunit/TestRunner.h"
+#include "bs_grid.hpp"
+
+
+class test_bs_grid : public CppUnit::TestCase{
+
+public:
+
+    test_bs_grid(std::string name)
+    :   TestCase(name) {}
+    
+
+    void test_
+
+}
+
+
+#endif
