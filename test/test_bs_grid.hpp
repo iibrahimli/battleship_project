3635d53c93027ab58bf83bbc1d602dc540a8fa64
iibrahimli <imranibrahimli98@gmail.com>
2019-03-26 21:45:24 +0400

    add empty bs_grid test functions

--- test/test_bs_grid.hpp
+++ test/test_bs_grid.hpp
@@ -2,11 +2,13 @@
 #define TEST_BS_GRID_HPP
 
 #include <string>
-#include "cppunit/TestCase.h"
-#include "cppunit/TestSuite.h"
-#include "cppunit/TestCaller.h"
-#include "cppunit/TestRunner.h"
+#include <cppunit/TestCase.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
 #include "bs_grid.hpp"
+#include "exceptions.hpp"
 
 
 class test_bs_grid : public CppUnit::TestCase{
@@ -15,11 +17,40 @@ public:
 
     test_bs_grid(std::string name)
     :   TestCase(name) {}
-    
 
-    void test_
 
-}
+    // test constructor
+    void test_constructor(){
+        
+    }
+
+
+    // test cell adressing
+    void test_cell_at(){
+        
+    }
+
+
+    // test ship placement
+    void test_place_ship(){
+
+    }
+
+
+    // test targeting and hits
+    void test_hit(){
+
+    }
+
+
+    CPPUNIT_TEST_SUITE(test_bs_grid);
+    CPPUNIT_TEST(test_constructor);
+    CPPUNIT_TEST(test_cell_at);
+    CPPUNIT_TEST(test_place_ship);
+    CPPUNIT_TEST(test_hit);
+    CPPUNIT_TEST_SUITE_END();
+
+};
 
 
 #endif
