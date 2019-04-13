0954535872937ede977192bb431f68d87b37c51b
iibrahimli <imranibrahimli98@gmail.com>
2019-04-13 17:33:10 +0400

    add skeleton for battleship game class

--- include/battleship.hpp
+++ include/battleship.hpp
@@ -1,4 +1,4 @@
-/*
+/*!
     Battleship game
 */
 
@@ -29,27 +29,34 @@ public:
     /*!
         @brief Constructor with grid size
 
-        Constructs a game with grid of given size
+        Constructs a game with grid of given size. A game holds 4 grids.
+        All of the grids are of the same size. Each players owns 2 of the grids.
+        For each player, one of the grid keeps track of their own ships and opponent's hits,
+        while the other grid keeps track of player's hits and sunken enemy ships.
+        Starting player becomes player A.
 
         @param width, height Dimensions of the grid
     */
     battleship(size_t width, size_t height)
     :   pa_hidden_grid(width, height),
-        pa_hit_grid(width, height),
+        pa_hit_grid   (width, height),
         pb_hidden_grid(width, height),
-        pb_hit_grid(width, height)
+        pb_hit_grid   (width, height),
+        pa_turn(true)
     {
-        
+
     }
 
 
 
 private:
-    bs_grid pa_hidden_grid;  ///< player a ship placement grid
-    bs_grid pa_hit_grid;     ///< player a hit tracking grid
-    bs_grid pb_hidden_grid;  ///< player b ship placement grid
-    bs_grid pb_hit_grid;     ///< player b hit tracking grid
-    bool p1_turn;            ///< current turn: player 1
+    bs_grid    pa_hidden_grid;  ///< player A ship placement grid
+    bs_grid    pa_hit_grid;     ///< player A hit tracking grid
+    bs_grid    pb_hidden_grid;  ///< player B ship placement grid
+    bs_grid    pb_hit_grid;     ///< player B hit tracking grid
+    bs_player  pa;              ///< player A
+    bs_player  pb;              ///< player B
+    bool       pa_turn;         ///< current turn: player A
 
 };
 
