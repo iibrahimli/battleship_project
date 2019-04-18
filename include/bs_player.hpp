3927fbc78c8e460a6c8d995d8e39a8600fdcb64e
iibrahimli <imranibrahimli98@gmail.com>
2019-04-18 21:58:51 +0400

    add random moves to bs_player base class

--- include/bs_player.hpp
+++ include/bs_player.hpp
@@ -92,7 +92,46 @@ public:
         this class will follow a random policy
     */
     virtual void move(){
-        // TODO random moves
+        int r, c;
+        std::pair<shot_result, int> sr;
+        bool valid_move = false;
+        static std::vector<ship_type> stype;
+        static int sindex = 0;
+        if(stype.size() == 0){
+            // add possible ship types to array
+            for(auto& p : hidden_grid->get_max_n_ships()){
+                for(int i=0; i<p.second; ++i)
+                    stype.push_back(p.first);
+            }
+        }
+        ship_orientation ori;
+
+        if(hidden_grid->is_ready()){
+            while(!valid_move){
+                r = rand() % hit_grid->get_height();
+                c = rand() % hit_grid->get_width();
+
+                try{
+                    sr = game->shoot_at(r, c);
+                    valid_move = true;
+                }
+                catch(illegal_move_exception& e){}
+            }
+        }
+        else{
+            while(!valid_move){
+                r = rand() % hit_grid->get_height();
+                c = rand() % hit_grid->get_width();
+                ori = rand() % 2 ? SO_HOR : SO_VERT;                
+
+                try{
+                    // TODO
+                    valid_move = game->place_ship(stype[sindex], r, c, ori);
+                    ++sindex;
+                }
+                catch(illegal_move_exception& e){}
+            }
+        }
     }
 
 
