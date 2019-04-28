d657a900ca1b6afe95bf63b6bf856468c6a5988b
iibrahimli <imranibrahimli98@gmail.com>
2019-04-28 17:56:48 +0400

    remove unnecessary test header

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@ int main(){
 
     srand(time(NULL));
     console_game game(10, 10, new human_player("Imran"), nullptr, OM_PA);
-    // console_game game(10, 10, nullptr, nullptr, OM_TXTONLY);
     game.start();
     
     return 0;
--- test/main.cpp
+++ test/main.cpp
@@ -2,11 +2,9 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/ui/text/TextTestRunner.h>
 #include "test_bs_grid.hpp"
-#include "test_battleship.hpp"
 
 
 CPPUNIT_TEST_SUITE_REGISTRATION(test_bs_grid);
-CPPUNIT_TEST_SUITE_REGISTRATION(test_battleship);
 
 
 int main(){
