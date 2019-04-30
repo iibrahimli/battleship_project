74112dca14c6efffb02e1fd853200ed7e7e6288c
iibrahimli <imranibrahimli98@gmail.com>
2019-04-30 23:24:04 +0400

    fix infinite loop when char is given

--- src/human_player.cpp
+++ src/human_player.cpp
@@ -26,15 +26,17 @@ void human_player::move(){
     }
 
     ship_type st;
-    int r, c;
+    size_t r, c;
     ship_orientation ori;
     std::pair<shot_result, int> sr;
     bool valid_move = false;
+    bool valid = false;
     int type;
     char orient = 'd';
 
     if(!hidden_grid->is_ready()){
         // placement
+        
         while(!valid_move){
             std::cout << "Enter ship type (length) (2, 3, 4 or 5), coordinates (row, col) and\n";
             std::cout << "orientation (h for horizontal, v for vertical)\n";
@@ -49,20 +51,33 @@ void human_player::move(){
             std::cin >> r;
             std::cin >> c;
             std::cin >> orient;
-            if(std::cin.eof()) throw std::runtime_error("Unexpected EOF");
+            if(std::cin.eof())
+                throw std::runtime_error("Unexpected EOF");
+            if(orient == '\n')
+                std::cout << std::endl;
+            if(hidden_grid->get_max_n_ships().find((ship_type) type) == hidden_grid->get_max_n_ships().end()){
+                std::cout << "[!] Unexpected ship type" << std::endl;
+                continue;
+            }
+            if(r >= hidden_grid->get_height() || c >= hidden_grid->get_width()){
+                std::cout << "[!] Unexpected indices" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(100, '\n');
+                continue;
+            }
 
             st = (ship_type) type;
 
             if(orient == 'V' || orient == 'v') ori = SO_VERT;
             else if(orient == 'H' || orient == 'h') ori = SO_HOR;
-            else { std::cout << "Invalid orientation" << std::endl; continue; }
+            else { std::cout << "[!] Invalid orientation" << std::endl; continue; }
 
             try{
                 valid_move = game->place_ship(st, r, c, ori);
-                if(!valid_move) std::cout << "Can't place that way" << std::endl;
+                if(!valid_move) std::cout << "[!] Can't place that way" << std::endl;
             }
             catch(illegal_move_exception& e){
-                std::cout << "Illegal move, try again" << std::endl;
+                std::cout << "[!] Illegal move, try again" << std::endl;
             }
         }
     }
@@ -75,16 +90,22 @@ void human_player::move(){
             std::cin >> r;
             std::cin >> c;
             if(std::cin.eof()) throw std::runtime_error("EOF");
+            if(r >= hidden_grid->get_height() || c >= hidden_grid->get_width()){
+                std::cout << "[!] Unexpected indices" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(100, '\n');
+                continue;
+            }
 
             try{
                 sr = game->shoot_at(r, c);
                 valid_move = true;
             }
             catch(illegal_move_exception& e){
-                std::cout << "Illegal move, try again" << std::endl;
+                std::cout << "[!] Illegal move, try again" << std::endl;
             }
             catch(index_exception& e){
-                std::cout << "Index out of bounds, try again" << std::endl;
+                std::cout << "[!] Index out of bounds, try again" << std::endl;
             }
         }
     }
